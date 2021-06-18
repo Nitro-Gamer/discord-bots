3fd2b119ac0ab824297311bee956020791004eab
Nitro Gamer <81626152+Nitro-Gamer@users.noreply.github.com>
2021-06-18 22:24:21 +0200

    Update index.js

--- Sprache: discord.js/STEP 1/index.js
+++ Sprache: discord.js/STEP 1/index.js
@@ -1,1 +1,29 @@
-soon
+const fs = require('fs');
+const discord = require('discord.js');
+
+const client = new discord.Client({ disableMentions: 'everyone' });
+
+client.config = require('./config.json');
+client.commands = new discord.Collection();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const messages = [`${client.config.prefix}help`];
+let current = 1;
+
+client.on('ready', () => {
+client.user.setActivity(messages[0] , {type: "PLAYING"})
+    setInterval(() => {
+        if(messages[current]){
+            client.user.setActivity(messages[current] , {type: "PLAYING"})
+            current++;
+        }else{
+            current = 0;
+            client.user.setActivity(messages[current] , {type: "PLAYING"})
+        }
+    }, 3*1000)
+});
+
+client.login(client.config.token);
